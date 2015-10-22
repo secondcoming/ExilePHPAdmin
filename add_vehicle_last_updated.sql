ebfcafbf0e6ddf5944067c5b34d0958632ed7e7d
second_coming <secondcoming@users.noreply.github.com>
2015-10-22 13:20:57 +0100

    Update add_vehicle_last_updated.sql

--- add_vehicle_last_updated.sql
+++ add_vehicle_last_updated.sql
@@ -1,1 +1,3 @@
-ALTER TABLE `vehicle` ADD COLUMN `last_updated` datetime DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP AFTER `pin_code`
+ALTER TABLE `vehicle` ADD COLUMN `last_updated` datetime DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP AFTER `pin_code`;
+
+UPDATE `vehicle` SET `last_updated` - now()
