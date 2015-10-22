9a37550377d55a56da96e86404b0d5dd73014af9
second_coming <secondcoming@users.noreply.github.com>
2015-10-22 13:22:20 +0100

    Update add_vehicle_last_updated.sql

--- add_vehicle_last_updated.sql
+++ add_vehicle_last_updated.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `vehicle` ADD COLUMN `last_updated` datetime DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP AFTER `pin_code`;
 
-UPDATE `vehicle` SET `last_updated` - now()
+// To populate the time run the following
+UPDATE vehicle SET last_updated = now()
