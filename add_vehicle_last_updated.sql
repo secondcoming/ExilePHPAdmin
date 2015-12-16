1e3e89801321c4c9a99a6870492ef2f2d7aa1de0
second_coming <secondcoming@users.noreply.github.com>
2015-12-16 14:04:30 +0000

    no longer needed since 0.9.40

--- add_vehicle_last_updated.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `vehicle` ADD COLUMN `last_updated` datetime DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP AFTER `pin_code`;
-
-// After adding the field to populate the time run the following
-UPDATE vehicle SET last_updated = now()
