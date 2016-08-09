84510d5a9c4d0a8fa9ab46727cad5a82cf8d1f6f
second_coming <secondcoming@users.noreply.github.com>
2016-08-09 13:24:27 +0100

    Updated for v1.0.0 Potato

--- exile_cleanup.php
+++ exile_cleanup.php
@@ -16,11 +16,32 @@
 	$date = date("Y-m-d_H-i-s");
 	$dumpfname = 'C:\\xampp\\htdocs\\db\\'.$dbname.'_'.$date.'.sql';
 	echo "<hr>$dumpfname<hr>";
-	$command = "C:\\xampp\\mysql\\bin\\mysqldump --add-drop-table --host=$dbhost --user=$dbuser --password=$dbpass  exile > $dumpfname"; 
+	$command = "C:\\xampp\\mysql\\bin\\mysqldump --add-drop-table --host=$dbhost --user=$dbuser --password=$dbpass  exile0940 > $dumpfname"; 
 	system($command);
 	
 	echo "backup $dumpfname made\n\n";
+	
+	
+	// delete dead players
+	$sql = "DELETE FROM player WHERE damage = 1";
+	$result = mysqli_query($db_local, $sql);
 
+	// Construction Cleanup
+	$sql = "DELETE FROM construction WHERE deleted_at IS NOT NULL";
+	$result = mysqli_query($db_local, $sql);
+
+	// Container Cleanup
+	$sql = "DELETE FROM territory WHERE deleted_at IS NOT NULL";
+	$result = mysqli_query($db_local, $sql);	
+
+	// Vehicle Cleanup
+	$sql = "DELETE FROM vehicle WHERE deleted_at IS NOT NULL";
+	$result = mysqli_query($db_local, $sql);
+	
+	// Territory Cleanup
+	$sql = "DELETE FROM territory WHERE deleted_at IS NOT NULL";
+	$result = mysqli_query($db_local, $sql);	
+	
 	// delete push bikes
 	$sql = "DELETE FROM vehicle WHERE class = 'Exile_Bike_OldBike' OR class = 'Exile_Bike_MountainBike'";
 	$result = mysqli_query($db_local, $sql);
@@ -36,10 +57,16 @@
 	// Remove old player history
 	$sql = "DELETE FROM player_history WHERE died_at <= NOW() - INTERVAL 24 HOUR";
 	$result = mysqli_query($db_local, $sql);
+
+	// Remove old players
+	$sql = "DELETE FROM player WHERE last_updated_at < NOW() - INTERVAL 14 DAY";
+	$result = mysqli_query($db_local, $sql);	
 	
 	// Remove constructions outside a territory and older than 48 hours
-	$sql = "DELETE FROM construction WHERE territory_id IS NULL AND last_updated_at < NOW() - INTERVAL 2 DAY";
+	$sql = "DELETE FROM construction WHERE territory_id IS NULL	AND last_updated_at < NOW() - INTERVAL 2 DAY";
 	$result = mysqli_query($db_local, $sql);	
-		
+	
+	// Delete Abandoned Vehicles
+	include 'delete_abandoned_vehicles.php';	
 
 ?>
